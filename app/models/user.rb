38d00863007c0a2e56a4739174f539813a069f4e
shou130521 <shou130521@gmail.com>
2026-04-13 22:33:49 +0900

    バリデーション＆ストロングパラメーターの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 正規表現
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  # ニックネーム・生年月日
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  # 名前（全角）
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+  end
+
+  # カナ（カタカナ）
+  with_options presence: true, format: { with: VALID_KANA_REGEX } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  # パスワード（英数字混合）
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 end
