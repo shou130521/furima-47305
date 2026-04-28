d3520baf3ceaa899c8c0fda711e576248d2de159
shou130521 <shou130521@gmail.com>
2026-04-28 22:37:21 +0900

    エラー表示の原因の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
-  belongs_to :delivery_day
+  belongs_to :shipping_day
 
   validates :name, presence: true
   validates :description, presence: true
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   validates :condition_id, numericality: { other_than: 1 }
   validates :shipping_fee_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
-  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
