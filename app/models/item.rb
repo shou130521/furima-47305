c0c0b3b87dc5465c9da0e944951ba6512ff1480d
shou130521 <shou130521@gmail.com>
2026-04-28 22:43:33 +0900

    ActiveStorage（画像投稿）

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+  has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
