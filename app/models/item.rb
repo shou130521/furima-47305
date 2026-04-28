7550ece198e69804eca4bb902e68d446c72d5681
shou130521 <shou130521@gmail.com>
2026-04-28 22:26:59 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
