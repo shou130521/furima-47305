09890e02a5680a12d78515b6a4cf4cfbb0bb5b74
shou130521 <shou130521@gmail.com>
2026-04-28 22:09:41 +0900

    ActiveHash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
