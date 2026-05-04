ffc592341112314190006ddbcd97db572bd8893f
shou130521 <shou130521@gmail.com>
2026-05-04 18:47:47 +0900

    Rubocopを実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Item, type: :model do
       it 'userが紐づいていなければ保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
 
       it '画像が空では保存できない' do
