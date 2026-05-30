4b49ba5686b7065241bd95b533988ebbfeb62885
shou130521 <shou130521@gmail.com>
2026-05-30 23:40:59 +0900

    購入フォーム機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,21 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
