9186bf7af20919e360ed6e4a799d471c922b79f7
shou130521 <shou130521@gmail.com>
2026-06-06 19:16:53 +0900

    本番環境のエラーの修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "card"
+pin "item_price"
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
